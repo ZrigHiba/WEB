2b62ca331d86bbfbb6dc09f976ba85b7564eb9dd
ZrigHiba <47651786+ZrigHiba@users.noreply.github.com>
2019-12-13 12:38:34 +0100

    Validation 3

--- ajouterReclamation.php
+++ ajouterReclamation.php
@@ -2,17 +2,20 @@
 include "../Entities/Reclamation.php";
 include "../Core/ReclamationC.php";
 
-if (isset($_POST['Nom'], $_POST['Email'], $_POST['Sujet'], $_POST['Msg'])) {
+if (isset($_POST['Nom'], $_POST['mail_to'], $_POST['mail_sub'], $_POST['mail_msg'])) {
     $reclamation1 = new Reclamation(
-        $_POST['Nom'],$_POST['Email'], $_POST['Sujet'], $_POST['Msg']
+        $_POST['Nom'],$_POST['mail_to'], $_POST['mail_sub'], $_POST['mail_msg']
     );
     $Reclamation1C = new ReclamationC();
     $Reclamation1C->AjouterReclamation($reclamation1);
+    $Reclamation1C->sendmail($_POST['mail_to']);
     header('Location: ../contact.html');
-} else {
+} else
+    {
     echo $_POST['Nom'];
-    echo $_POST['Email'];
-    echo $_POST['Sujet'];
-    echo $_POST['Msg'];
+    echo $_POST['mail_to'];
+    echo $_POST['mail_sub'];
+    echo $_POST['mail_msg'];
     echo "vérifier les champs";
-}?>
+}
+?>
